6ec5f110a5dfa9cea8c29f435c0c7fef89cece04
pobydoby <pabitra.aryanz@gmail.com>
2021-12-14 13:54:10 -0600

    updated the mailchimp connection

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+	var firstName = req.body.fname;
+	var lastName = req.body.lname;
+	var email = req.body.email;
+
+	console.log(firstName, lastName, email);
+});
+
+app.listen(3000, function () {
+	console.log("The server is listening on port 3000!")
+});
+
+//apiKey:
+// e2a14940c17e9c7053aa09276d9b651e - us20
+
+//audience id : 
+// a67d46db12
