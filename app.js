e62a7f53a5af511ee2fdf57cec717171c6c403d6
pobydoby <pabitra.aryanz@gmail.com>
2021-12-15 23:21:13 -0600

    success and failure setup

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+//const request = require("request");
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
@@ -13,12 +14,50 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-	var firstName = req.body.fname;
-	var lastName = req.body.lname;
-	var email = req.body.email;
+	const firstName = req.body.fname;
+	const lastName = req.body.lname;
+	const email = req.body.email;
 
-	console.log(firstName, lastName, email);
+	const data = {
+		members: [{
+			email_address: email,
+			status: "subscribed",
+			merge_fields: {
+				FNAME: firstName,
+				LNAME: lastName
+			}
+		}
+
+		]
+	};
+
+	const jsonData = JSON.stringify(data);
+	const url = "https://us20.api.mailchimp.com/3.0/lists/a67d46db12"
+	const options = {
+		method: "POST",
+		auth: "pabitra:e2a14940c17e9c7053aa09276d9b651e-us20"
+	}
+
+	const request = https.request(url, options, function (response) {
+		
+		if (response.statusCode === 200) {
+			res.sendFile(__dirname + "/success.html");
+		} else {
+			res.sendFile(__dirname + "/failure.html");
+		}
+
+		response.on("data", function(data) {
+			console.log(JSON.parse(data));
+		})
+	})
+
+	request.write(jsonData);
+	request.end()
 });
+
+app.post("/failure", function(req, res) {
+	res.redirect("/");
+})
 
 app.listen(3000, function () {
 	console.log("The server is listening on port 3000!")
@@ -27,5 +66,5 @@ app.listen(3000, function () {
 //apiKey:
 // e2a14940c17e9c7053aa09276d9b651e - us20
 
-//audience id : 
+//audience id :
 // a67d46db12
